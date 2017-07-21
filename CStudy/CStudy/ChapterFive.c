3184e3452de4f249b41d96cd23ef4c2928ea54dd
l'm wonder <200916000@qq.com>
2017-07-21 21:54:55 +0800

    循环复习完成，GOGO加油！

--- CStudy/CStudy/ChapterFive.c
+++ CStudy/CStudy/ChapterFive.c
@@ -1,14 +1,49 @@
 //运算符，表达式和语句
 #include <stdio.h>
 
-int main(void)
+int ChapterFive(void)
 {
 	//左值，  用于标识特定数据对象的名称和表达式， 可修改的左值（const创建不可修改的）或者使用术语对象定位值
 	//右值，  能赋值给可修改左值得量，且本身不是左值
+	//整数除法小数点部分截断
+	//结合律只适用于共享同一运算对象运算符，
+	//求模运算符只能用于整数
+	//   % 求模   等于    a - (a/b)*b        标准规定
+	//递增递减运算符只能影响一个变量（或者说，只能影响一个可修改的左值）
 
 
+	//--------------------------sizeof运算符和size_t类型---------------------------
+	/*
+	int n = 0;
+	size_t intsize;
+	intsize = sizeof(int); //sizeof 返回size_t类型的值 他是无符号整数类型
+	
+	printf("n = %d, n has %zd bytes; all ints have %zd bytes. \n", n, sizeof n, intsize);
+		*/
+
+	//--------------------------C转换规则------------------------------------------
+
+	//当类型转换出现在表达式的时，无论是unsigned,或signed  char  short 都会被转换成int 
+	//有时候也会被转换成unsigned int 比如short和int一样大的时候
+	//小到大，升级 （或自动类型转换）
+
+	//两种类型运算，会被转换成两种类型的更高级别
 
 
+	//		自动类型转换
+	char ch;
+	int i;
+	float f1;
+
+	f1 = i = ch = 'c';
+	printf("ch = %c, i = %d, float = %2.2f \n", ch, i, f1);
+	ch = ch + 1;
+	i = f1 + 2 * ch;
+	printf("ch = %c, i = %d, float = %2.2f \n", ch, i, f1);
+	ch = 1107;
+	printf("Now ch = %c \n", ch);
+	ch = 80.89;
+	printf("Now ch = %c \n", ch);
 
 
 
---
+++ CStudy/CStudy/ChapterSix.c
@@ -0,0 +1,77 @@
+//控制语句   循环
+#include <stdio.h>
+#include <math.h> // C数学库
+
+int main(void)
+{
+	//--------------------------------While循环---------------------------------------
+	/*
+	long num;
+	long sum = 0L;
+	//int status;
+
+	printf("Please enter an  integer to be summed ");
+	printf("q to quit :");
+	//status = scanf("%ld", &num);
+
+	while (scanf("%ld", &num) == 1) //status == 1
+	{
+		sum += num;
+		printf("Please enter next integer (Q to quit) : ");
+		//status = scanf("%ld", &num);
+
+	}
+	printf("Those integers sum to %ld.\n", sum);  */
+
+	//--------------------------------fabs函数比较浮点数(math.h)---------------------
+	/*
+	const double ANSWER = 3.14159;
+	double response;
+
+	printf("What is the value of pi?\n");
+	scanf("%lf", &response);
+
+	while (fabs(response - ANSWER) > 0.0001)  //用户输入的值与正确的值相差0.0001循环结束
+	{
+		printf("Try again! \n");
+		scanf("%lf", &response);
+	}
+	printf("Close enough! \n");      */
+
+	//-------------------------------for循环---------------------------------------------
+	/*
+	const int FIRST_OZ = 46;
+	const int NEXT_OZ = 20;
+	int ounces, cost;
+
+	for (ounces = 1, cost = FIRST_OZ;ounces <= 16; ounces++, cost += NEXT_OZ)
+			printf("%5d  $%4.2f\n", ounces, cost / 100.0);
+	//for(initializa;   test;    update)
+	               //statement
+	//initializa循环开始前只执行一次，test测试条件，update更新步长  可用逗号分隔增加表达式，号是一个序列点
+	//整个逗号表达式的值是右侧项的值
+	*/
+
+	//------------------------------zeno悖论（箭永远射不到目标）---------------------
+	/*
+	int   t_ct;
+	double   time, power_of_2;
+	int   limit;
+	printf("Enter the number of terms you want: ");
+	scanf("%d", &limit);
+	for (time = 0, power_of_2 = 1, t_ct = 1; t_ct <= limit; t_ct++, power_of_2 *= 2.0)
+	{
+		time += 1.0 / power_of_2;
+		printf("time = %lf  when terms = %d. \n", time, t_ct);
+
+	}
+	//数学上简单的方法可用      */
+
+	//-------------------------------数组，函数----------------------------------------
+
+
+
+
+
+	return 0;
+}
